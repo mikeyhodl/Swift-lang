230a4e30424386c76e816c80cb7e8931436b9d3a
mikeowino <mike.owino40@yahoo.com>
2020-12-15 00:15:04 +0300

    es

--- Logical AND Operator.swift
+++ Logical AND Operator.swift
@@ -7,3 +7,26 @@ false && true	false
 false && false	false
 
 */
+
+var midnight = true 
+var date = "January 1, 2020"
+
+if midnight && date == "January 1, 2020" {
+  print("It's the start of a new decade!") 
+}
+
+/*
+var time = 8 // PM 
+var phoneInUse = true 
+var brightness: Double
+ 
+if time >= 8 && phoneInUse {
+  brightness = 0.75 
+} else {
+  brightness = 1 
+}
+ 
+print(brightness) // Prints: 0.75
+
+*/
+
