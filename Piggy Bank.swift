bf18b8f2b42012cf21b31a3b1e1d8352135e93d5
mikeowino <mike.owino40@yahoo.com>
2020-12-14 18:45:44 +0300

    // >>> $53.0

---
+++ Piggy Bank.swift
@@ -0,0 +1,17 @@
+var pesos: Double = 100
+var reais: Double = 100
+var soles: Double = 100
+
+var total: Double
+
+// 🇨🇴 Pesos conversion rate: 0.050
+// 🇧🇷 Reais conversion rate: 0.20
+// 🇵🇪 Soles conversion rate: 0.28
+
+// as at Mon 14 Dec 18:43:55
+
+total = 0.050 * pesos + 0.20 * reais + 0.28 * soles
+
+print("Total USD = $\(total)")
+
+// >>> $53.0
