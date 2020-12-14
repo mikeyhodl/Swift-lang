1135e870432a3230b5f81b98e11481a215652d2e
mikeowino <mike.owino40@yahoo.com>
2020-12-14 13:08:28 +0300

    print("Dear Self,") print("I embarked on my programming journey in 2009 when I enrolled in a CS course in college, not knowing what it meant nor where it could possibly take me.In the last ten years, programming has brought tremendous joy to my life and I met so many wonderful people through it. I am incredibly jealous of where you are right now because I would love to experience this all over again.Thank you for learning Swift with us. I can’t wait to see what you create with it. Mike @ Codecademy.") print("Mike") print("1/1/2020, New York")

---
+++ letter.swift
@@ -0,0 +1,4 @@
+print("Dear Self,")
+print("I embarked on my programming journey in 2009 when I enrolled in a CS course in college, not knowing what it meant nor where it could possibly take me.In the last ten years, programming has brought tremendous joy to my life and I met so many wonderful people through it. I am incredibly jealous of where you are right now because I would love to experience this all over again.Thank you for learning Swift with us. I can’t wait to see what you create with it. Mike @ Codecademy.")
+print("Mike")
+print("1/1/2020, New York")
