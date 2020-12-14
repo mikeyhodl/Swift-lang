998581e1f866a4fa6bc3d1a037454a212c7f0a88
mikeowino <mike.owino40@yahoo.com>
2020-12-14 23:51:51 +0300

    es

---
+++ Switch Statement: where Clause.swift
@@ -0,0 +1,25 @@
+var wholeNumber = 15 
+
+/*
+var randomNumber = Int.random(in: 0...10)
+ 
+switch randomNumber {
+  case let x where x % 2 == 0:
+    print("\(randomNumber) is even")
+  case let x where x % 2 == 1:
+    print("\(randomNumber) is odd")
+  default:
+    print("Invalid")
+}
+
+switch randomNumber {
+  case let x where x % 2 == 0:
+    print("\(randomNumber) is even")
+  case let x where x % 2 == 1:
+    print("\(randomNumber) is odd")
+  default:
+    print("Invalid")
+}
+ 
+// Prints: 7 is odd
+*/
