18a2b11bb43d3e4dea24dd06a30fb55aad94bbcd
mikeowino <mike.owino40@yahoo.com>
2020-12-14 15:11:57 +0300

    var apples = 16 print(apples)
    /*
    += add and assign the sum.
    -= subtract and assign the difference.
    *= multiply and assign the product.
    /= divide and assign the quotient.
    %= divide and assign the remainder.
    
    var dollars = 5
    dollars += 4   // same as dollars = 5 + 4
    print(dollars) // Prints: 9
    
    dollars -= 3   // same as dollars = 9 - 3
    print(dollars) // Prints: 6
    
    dollars *= 5   // same as dollars = 6 * 5
    print(dollars) // Prints: 30
    
    dollars /= 6   // same as dollars = 30 / 6
    print(dollars) // Prints: 5
    
    dollars %= 2   // same as dollars = 5 % 2
    print(dollars) // Prints: 1
    */

---
+++ assignment.swift
@@ -0,0 +1,29 @@
+var apples = 16
+print(apples)
+
+
+/*
++= add and assign the sum.
+-= subtract and assign the difference.
+*= multiply and assign the product.
+/= divide and assign the quotient.
+%= divide and assign the remainder.
+
+var dollars = 5
+dollars += 4   // same as dollars = 5 + 4
+print(dollars) // Prints: 9
+ 
+dollars -= 3   // same as dollars = 9 - 3
+print(dollars) // Prints: 6
+ 
+dollars *= 5   // same as dollars = 6 * 5
+print(dollars) // Prints: 30
+ 
+dollars /= 6   // same as dollars = 30 / 6
+print(dollars) // Prints: 5
+ 
+dollars %= 2   // same as dollars = 5 % 2
+print(dollars) // Prints: 1
+*/
+
+
