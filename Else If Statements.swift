d2b0e845aef047e6bb9569d02c95aaca1528c77c
mikeowino <mike.owino40@yahoo.com>
2020-12-14 23:13:35 +0300

    es

---
+++ Else If Statements.swift
@@ -0,0 +1,42 @@
+/*
+let grade = 85
+let letterGrade: String 
+ 
+if grade >= 90 {
+  letterGrade = "A"
+} else if grade >= 80 {
+  letterGrade = "B"
+} else if grade >= 70 {
+  letterGrade = "C"
+} else if grade >= 60 {
+  letterGrade = "D"
+} else if grade < 60 {
+  letterGrade = "F"
+} else {
+  letterGrade = "N/A"
+}
+ 
+print(letterGrade) 
+// Prints: B
+*/
+
+/*
+English: EN
+Spanish: ES
+Chinese: ZH
+Russian: RU
+*/ 
+
+var abbreviation = "RU"
+
+if abbreviation == "EN" {
+  print("English")
+} else if abbreviation == "ES" {
+  print("Spanish")
+} else if abbreviation == "ZH" {
+ print("Chinese")
+} else if abbreviation == "RU" {
+ print("Russian")
+} else {
+ print("Abbreviation not found") 
+}
