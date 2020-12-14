043230ab609d00af3a615f8f4e1ccff0ca32cffe
mikeowino <mike.owino40@yahoo.com>
2020-12-14 18:33:28 +0300

    // Dog Years 🐶

---
+++ dog_years.swift
@@ -0,0 +1,13 @@
+// Dog Years 🐶
+// Sonny Li
+var dogAge: Double = 2.5
+
+let earlyYears: Double = 21.0
+var laterYears: Double = 0.0
+var humanYears: Double = 0.0
+
+laterYears = (dogAge - 2) * 4
+
+humanYears = earlyYears + laterYears
+
+print("Ruff Ruff! My name is Landon and I'm a Shiba Inu. I'm \(humanYears) years old in human years.")
