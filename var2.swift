32f71d920a8642a173387c87d7890f0a0b76ae69
mikeowino <mike.owino40@yahoo.com>
2020-12-14 13:34:12 +0300

    // Write your code below 🧾 /* + addition - subtraction * multiplication / division % modulo (divides and gives the remainder)
    var score = 0
    // score is 0
    
    score = 4 + 2
    // it is now 6
    
    score = score - 2
    // it is now 4
    
    score = 4 * 2
    // it is now 8
    
    score = 4 / 2
    // and now 2
    
    score = 5 % 2
    // and now 1
    */
    var amount = 500.0
    
    var tip = amount * 0.5
    
    print(tip)

---
+++ var2.swift
@@ -0,0 +1,33 @@
+// Write your code below 🧾
+/*
++ addition
+- subtraction
+* multiplication
+/ division
+% modulo (divides and gives the remainder)
+
+var score = 0
+// score is 0
+ 
+score = 4 + 2
+// it is now 6
+ 
+score = score - 2
+// it is now 4
+ 
+score = 4 * 2
+// it is now 8
+ 
+score = 4 / 2
+// and now 2
+ 
+score = 5 % 2
+// and now 1
+*/
+var amount = 500.0
+
+var tip = amount * 0.5
+
+print(tip)
+
+
