b526eee4bcdfd0e2a0b4214b0ddbd231807381b6
mikeowino <mike.owino40@yahoo.com>
2020-12-14 23:32:11 +0300

    es

---
+++ Switch Statement.swift
@@ -0,0 +1,47 @@
+var secretIdentity = "Tony Stark"
+var superheroName: String 
+ 
+
+switch secretIdentity {
+  case "Tony Stark": 
+    superheroName = "Iron Man"
+  case "Natasha Romanoff":
+    superheroName = "Black Widow"
+  case "Prince T'Challa":
+    superheroName = "Black Panther"
+  case "Thor":
+    superheroName = "Thor" 
+  default:
+    superheroName = "Unknown"
+} 
+
+print(superheroName)
+
+// Write your code below
+/*
+var city = "Rome"
+ 
+if city == "Rapa Nui" { 
+  print("Moai 🗿")
+} else if city == "New York" {
+  print("Statue of Liberty 🗽")
+} else if city == "Rome" {
+  print("Colosseum 🏛")
+} else {
+  print("A famous landmark is the Eiffel Tower!")
+}
+
+switch city {
+  case "Rapa Nui":
+    print("Moai 🗿")
+  case "New York":  
+    print("Statue of Liberty 🗽")
+  case "Rome":
+    print("Colosseum 🏛")
+  default: 
+    print("A famous landmark is the Eiffel Tower!")
+}
+*/
+
+
+
