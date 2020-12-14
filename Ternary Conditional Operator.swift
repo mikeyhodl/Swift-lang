39236fafdd851abcc17b82c304fedfe45e8c4b65
mikeowino <mike.owino40@yahoo.com>
2020-12-14 23:22:37 +0300

    es

---
+++ Ternary Conditional Operator.swift
@@ -0,0 +1,17 @@
+var windy = true 
+
+windy ? print("Sails up") : print("Motor on")
+
+// $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+/*
+var orderSuccessfullyPlaced = false 
+ 
+if orderSuccessfullyPlaced {
+  print("Your order was received.")
+} else {
+  print("Something went wrong.")
+}
+
+*/
+
