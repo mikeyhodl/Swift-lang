1aa0238847a610568c6522a67e84fd520e4c8b77
mikeowino <mike.owino40@yahoo.com>
2020-12-14 15:18:08 +0300

    /* Int: integer numbers Double: floating-point numbers String: a sequence of characters Bool: true/false values
    */
    
    var book: String = "Just Kids"
    
    var pageCount: Int = 278

---
+++ type.swift
@@ -0,0 +1,12 @@
+/*
+Int: integer numbers
+Double: floating-point numbers
+String: a sequence of characters
+Bool: true/false values
+
+
+*/
+
+var book: String = "Just Kids"
+
+var pageCount: Int = 278
