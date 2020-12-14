183faa6d7d7834049102366768b7503be81b1abf
mikeowino <mike.owino40@yahoo.com>
2020-12-14 21:04:45 +0300

    /*

---
+++ Comparison Operators...if..else.swift
@@ -0,0 +1,33 @@
+let tuneSquadPoints = 78
+let monstarsPoints = 77
+
+if tuneSquadPoints < monstarsPoints {
+  print("Monstars are the winners of today's game!")
+} else {
+  print("tuneSquad are the winners of today's game!")
+}
+/*
+== Equal to
+!= Not Equal to
+> Greater than
+< Less than
+>= Greater than or equal to
+<= Less than or equal to
+
+4 < 5        // true 
+0.5 > 0.1    // true
+3.5 <= 3.0   // false
+12 >= 15     // false
+"A" == "A"   // true 
+"B" != "b"   // true
+
+let grade = 95
+ 
+if grade > 65 {
+  print("You passed!") 
+} else {
+  print("You failed.")
+}
+*/
+
+
