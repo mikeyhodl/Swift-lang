e9492d7b48670c99c67a74d4e6aee9e8a437bcf8
mikeowino <mike.owino40@yahoo.com>
2020-12-17 10:15:59 +0300

    loop

---
+++ The for-in Loop.swift
@@ -0,0 +1,14 @@
+// The start of the Happy Birthday song 🎂
+print("Happy Birthday to you.")
+print("Happy Birthday to you!")
+print("Happy Birthday dear Mike.")
+print("Happy Birthday to you!!")
+print(".......................")
+
+// Add your code below 🥳
+for age in 1...35 {
+  print("Are you \(age)?")
+}
+
+
+
