6db7ad42d073032b64266c34cc8544a8ef18edb5
Mike Owino <mike.owino40@yahoo.com>
2020-12-19 16:27:28 +0300

    Create FUNCTIONS Multiple Parameters.swift

---
+++ FUNCTIONS Multiple Parameters.swift
@@ -0,0 +1,8 @@
+// Write your code below 🛫
+
+func timeToDestination(distance: Int, speed: Int) -> Int {
+  let time = distance / speed
+  return time
+}
+
+print(timeToDestination(distance: 6836, speed: 560))
