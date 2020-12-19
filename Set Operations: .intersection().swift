f3b0834462e561ab0db2bfcef630b675f79e9350
Mike Owino <mike.owino40@yahoo.com>
2020-12-19 15:41:33 +0300

    Create Set Operations: .intersection().swift

---
+++ Set Operations: .intersection().swift
@@ -0,0 +1,9 @@
+var swim: Set = ["Turtles", "Ducks", "Puffins", "Shark"]
+
+var fly: Set = ["Humming birds", "Bats", "Ducks", "Puffins"]
+
+// Add your code below 🦆
+
+let swimAndFly = swim.intersection(fly)
+
+print(swimAndFly)
