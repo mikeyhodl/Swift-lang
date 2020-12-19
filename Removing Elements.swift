9dbec9ac39a3d8c8be22e4e5aee160b6857b8a3b
Mike Owino <mike.owino40@yahoo.com>
2020-12-19 15:39:29 +0300

    Create Removing Elements.swift

---
+++ Removing Elements.swift
@@ -0,0 +1,8 @@
+var planets: Set = ["Earth", "Jupiter", "Mars", "Neptune", "Saturn", "Venus", "Uranus", "Mercury", "Pluto"]
+
+// Write your code below ⭐
+
+planets.remove("Pluto")
+print(planets)
+planets.removeAll()
+print(planets)
