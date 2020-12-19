b14340acb17f261a320a2235f044ccae2b88d1f9
Mike Owino <mike.owino40@yahoo.com>
2020-12-19 16:23:41 +0300

    Create FUNCTIONS Calling a Function.swift

---
+++ FUNCTIONS Calling a Function.swift
@@ -0,0 +1,13 @@
+// Write your code below 🍲
+
+func mySpecialRecipe() {
+ print("Shred 4 large carrots into bowl")
+ print("Cut prunes, dried apricots, raisins, and walnuts into tiny pieces")
+ print("Add to bowl and mix")
+ print("Form cutlets")
+ print("Dip each cutlet into farina") 
+ print("Fry cutlets on the pan with coconut oil 2 mins on each side") 
+ print("Serve the carrot cutlets warm! 🥕") 
+} 
+
+mySpecialRecipe()
