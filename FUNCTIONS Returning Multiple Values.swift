ac085e2bd75354187a956f4b8591410aa8cc25e9
Mike Owino <mike.owino40@yahoo.com>
2020-12-19 16:29:25 +0300

    Create FUNCTIONS Returning Multiple Values.swift

---
+++ FUNCTIONS Returning Multiple Values.swift
@@ -0,0 +1,9 @@
+// Write your code below 😋
+
+func favoriteCuisine() -> (name: String, dish: String) {
+ return (name: "Russian", dish: "Pelmeni") 
+}
+
+let cuisine = favoriteCuisine()
+
+print("My favorite \(cuisine.name) dish is \(cuisine.dish)!")
