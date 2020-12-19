2ab6936f17b4eb00f7a7e9f513971c7984760506
Mike Owino <mike.owino40@yahoo.com>
2020-12-19 15:28:25 +0300

    Create .append() Method.swift

---
+++ .append() Method.swift
@@ -0,0 +1,10 @@
+var resolutions = ["play more music 🎸",
+                   "read more books 📚",
+                   "drink more water 💧"]
+
+// Write your code below 💪
+
+resolutions.append("cook more food 🍝")
+resolutions += ["save more money 💰"]
+
+print(resolutions)
