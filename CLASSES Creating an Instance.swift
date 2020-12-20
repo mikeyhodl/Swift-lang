0d4500dc442e5af27506e2e2eed9c3e4ad324df7
Mike Owino <mike.owino40@yahoo.com>
2020-12-20 14:47:10 +0300

    Create CLASSES Creating an Instance.swift

---
+++ CLASSES Creating an Instance.swift
@@ -0,0 +1,20 @@
+class Restaurant {
+  var name = ""
+  var type = [""]
+  var rating = 0.0
+  var delivery = false
+}
+
+// Write your code below 🍝
+
+var bobsBurgers = Restaurant()
+
+bobsBurgers.name = "Bob's Burgers"
+bobsBurgers.type = ["Burgers", "Fast Food"]
+bobsBurgers.rating = 3.5
+bobsBurgers.delivery = true
+
+print(bobsBurgers.name)
+print(bobsBurgers.type)
+print(bobsBurgers.rating)
+print(bobsBurgers.delivery)
