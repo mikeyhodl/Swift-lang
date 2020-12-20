11af7ab5ec9d610a7c8f74560d982482a672f1dc
Mike Owino <mike.owino40@yahoo.com>
2020-12-20 14:31:05 +0300

    Create STRUCTURES Accessing and Editing Properties.swift

---
+++ STRUCTURES Accessing and Editing Properties.swift
@@ -0,0 +1,17 @@
+struct Book {
+  var pages = 0
+  var title = ""
+}
+
+var myFavBook = Book()
+
+// Write your code below: 📚
+print(myFavBook.pages)
+
+myFavBook.pages = 640
+
+print(myFavBook.pages)
+
+myFavBook.title = "The Count of Monte Cristo"
+
+print(myFavBook.title)
