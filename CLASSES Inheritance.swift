dd736f9e6115a1a2bae14abf2142b3f1fb3f8691
Mike Owino <mike.owino40@yahoo.com>
2020-12-20 14:48:56 +0300

    Create CLASSES Inheritance.swift

---
+++ CLASSES Inheritance.swift
@@ -0,0 +1,19 @@
+class Order {
+  var items = [""]
+  var subtotal = 0.0
+  var tip = 0.0
+  var total = 0.0
+
+  func printReceipt() {
+    print("Items:     \(items)")
+    print("Subtotal:  $\(subtotal)")
+    print("Tip:       $\(tip)")
+    print("Total:     $\(total)")
+  }
+}
+
+// Write your code below 🧾
+
+class DeliveryOrder: Order {
+  var deliveryFee = 2.0
+}
